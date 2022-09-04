ef95dcd4641c1f660e3c1ae993b6606df53dcc16
Captain-Murasa <a109700504@gmail.com>
2022-09-04 16:27:42 +0800

    Complete project4.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,51 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    // initialization
+    @SCREEN
+    D=A
+    @addr
+    M=D     // addr = 16384
+
+    @KBD
+    D=M
+    @PRESSED
+    D;JNE   // if pressed goto PRESSED
+
+    // no key is pressed
+(NOTPRESSED)
+    @addr
+    A=M 
+    M=0    // RAM[addr]=0
+
+    @addr
+    M=M+1
+    D=M     // addr++
+
+    @KBD
+    D=A-D
+    @NOTPRESSED
+    D;JNE   // if addr < 24576 goto PRESSED
+
+    @END
+    0;JMP   // if done, goto END
+
+(PRESSED)
+    @addr
+    A=M 
+    M=-1    // RAM[addr]=1111111111111111
+
+    @addr
+    M=M+1
+    D=M     // addr++
+
+    @KBD
+    D=A-D
+    @PRESSED
+    D;JNE   // if addr < 24576 goto PRESSED 
+
+(END)
+    // infinite loop
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,34 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    // initialization. i = 0, R2 = 0
+    @i
+    M=0
+    @R2
+    M=0
+
+(LOOP)
+    // if i == R0 goto END
+    @i
+    D=M
+    @R0
+    D=M-D
+    @END
+    D;JEQ
+
+    // R2 += R1
+    @R1
+    D=M
+    @R2
+    M=D+M
+
+    // i++
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
